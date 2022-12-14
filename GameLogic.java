02acff8e63ae2b9cde1f1ea7783b1bffed29b5c6
Viktoriia Rybalka <69701982+Sonorhc314@users.noreply.github.com>
2022-12-14 01:08:23 +0000

    3rd

--- GameLogic.java
+++ GameLogic.java
@@ -56,6 +56,7 @@ public class GameLogic {
 						current_col_bot = rand_col;
 						break;
 				}
+				break;
 			}
 		}
 	}
--- tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-if(current_gold == all_gold && original_map[current_row][current_col]=='E')
-		{
-			System.out.println("WIN");
-		}
-		else
-		{
-			System.out.println("LOOSE");
-		}
-		System.exit(0);
